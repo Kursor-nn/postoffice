86eec5e4409fe067def8d6bdb70cab0008a10a1b
Aleksandr Kozachuk <alko0516@netcracker.com>
2020-02-07 13:59:35 +0000

    Tests are added

--- src/main/java/com/kozachuk/homework/postoffice/dto/PackageBox.java
+++ src/main/java/com/kozachuk/homework/postoffice/dto/PackageBox.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PackageBox {
+    private List<Double> sizes = null;
     private double length;
     private double width;
     private double depth;
 
-    public PackageBox(double length, double width, double depth){
+    private PackageBox(double length, double width, double depth){
         this.length = length;
         this.width = width;
         this.depth = depth;
+    }
+
+    public static BoxBuilder getBuilder() {
+        return new PackageBox.BoxBuilder();
     }
 
     public double getLength() {
@@ -27,10 +32,42 @@ public class PackageBox {
     }
 
     public List<Double> getSizes() {
-        return new ArrayList(){{
-            add(getWidth());
-            add(getLength());
-            add(getDepth());
-        }};
+        if(sizes == null) sizes = new ArrayList(){{
+                                        add(getWidth());
+                                        add(getLength());
+                                        add(getDepth());
+                                    }};
+
+        return sizes;
+    }
+
+    public static class BoxBuilder {
+        private double length;
+        private double width;
+        private double depth;
+
+        private BoxBuilder(){}
+
+        public BoxBuilder length(double length) {
+            if(length <= 0) throw new IllegalArgumentException(String.format("Illegal Argument value '%s'", length));
+            this.length = length;
+            return this;
+        }
+
+        public BoxBuilder width(double width) {
+            if(width <= 0) throw new IllegalArgumentException(String.format("Illegal Argument value '%s'", width));
+            this.width = width;
+            return this;
+        }
+
+        public BoxBuilder depth(double depth) {
+            if(depth <= 0) throw new IllegalArgumentException(String.format("Illegal Argument value '%s'", depth));
+            this.depth = depth;
+            return this;
+        }
+
+        public PackageBox build() {
+            return new PackageBox(length, width, depth);
+        }
     }
 }
--- src/test/java/postoffice/PostOfficeTest.java
+++ src/test/java/postoffice/PostOfficeTest.java
@@ -10,62 +10,100 @@ public class PostOfficeTest {
 
     @Test
     public void shouldAnswerWithFalse() {
-        PackageBox box        = new PackageBox(1, 1, 1);
-        PackageBox packageBox = new PackageBox(1, 1, 1);
+        PackageBox box = PackageBox.getBuilder().length(1).width(1).depth(1).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(1).width(1).depth(1).build();
+
         Assert.assertFalse(office.validateSize(box, packageBox));
     }
 
     @Test
     public void shouldAnswerWithFalse_packageBoxIsSmaller() {
-        PackageBox box        = new PackageBox(0.5, 1, 1);
-        PackageBox packageBox = new PackageBox(1, 1, 1);
+        PackageBox box = PackageBox.getBuilder().length(0.5).width(1).depth(1).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(1).width(1).depth(1).build();
+
         Assert.assertFalse(office.validateSize(box, packageBox));
     }
 
     @Test
     public void shouldAnswerWithTrue_boxIsBigger() {
-        PackageBox box        = new PackageBox(2, 2, 2);
-        PackageBox packageBox = new PackageBox(1, 1, 1);
+        PackageBox box = PackageBox.getBuilder().length(2).width(2).depth(2).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(1).width(1).depth(1).build();
+
         Assert.assertTrue(office.validateSize(box, packageBox));
     }
 
     @Test
     public void shouldAnswerWithFalse_boxIsTheSame_byWidth() {
-        PackageBox box        = new PackageBox(2, 3, 4);
-        PackageBox packageBox = new PackageBox(3.5, 3, 1);
+        PackageBox box        = PackageBox.getBuilder().length(2).width(3).depth(4).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(3.5).width(3).depth(1).build();
+
         Assert.assertFalse(office.validateSize(box, packageBox));
     }
 
     @Test
     public void shouldAnswerWithFalse_boxIsTheSame() {
-        PackageBox box        = new PackageBox(2, 3, 4);
-        PackageBox packageBox = new PackageBox(4, 3, 3);
+        PackageBox box        = PackageBox.getBuilder().length(2).width(3).depth(4).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(4).width(3).depth(3).build();
+
+        Assert.assertFalse(office.validateSize(box, packageBox));
+    }
+
+    @Test
+    public void shouldAnswerWithFalse_boxIsTheSame_2() {
+        PackageBox box        = PackageBox.getBuilder().length(1).width(2).depth(4).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(1).width(2).depth(2).build();
+
         Assert.assertFalse(office.validateSize(box, packageBox));
     }
 
     @Test
     public void shouldAnswerWithFalse_boxIsBiggerByWidth() {
-        PackageBox box        = new PackageBox(2, 5, 4);
-        PackageBox packageBox = new PackageBox(4, 3, 2);
+        PackageBox box        = PackageBox.getBuilder().length(2).width(4).depth(4).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(4).width(3).depth(2).build();
+
         Assert.assertFalse(office.validateSize(box, packageBox));
     }
 
     @Test
     public void packageIsSmaller() {
-        PackageBox box        = new PackageBox(5, 4, 3);
-        PackageBox packageBox = new PackageBox(2, 3, 4);
+        PackageBox box        = PackageBox.getBuilder().length(5).width(4).depth(3).build();
+        PackageBox packageBox = PackageBox.getBuilder().length(2).width(3).depth(4).build();
+
         Assert.assertTrue(office.validateSize(box, packageBox));
     }
 
     @Test(expected = NullPointerException.class)
     public void boxIsNull_exception_NPE() {
-        PackageBox packageBox = new PackageBox(3, 2, 1);
+        PackageBox packageBox = PackageBox.getBuilder().length(2).width(3).depth(4).build();
         office.validateSize(null, packageBox);
     }
 
     @Test(expected = NullPointerException.class)
     public void pacakgeIsNull_exception_NPE() {
-        PackageBox box = new PackageBox(3, 2, 1);
+        PackageBox box = PackageBox.getBuilder().length(5).width(4).depth(3).build();
         office.validateSize(box, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exception_iae_length_0() {
+        PackageBox.getBuilder().length(0).width(1).depth(1).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exception_iae_width_0() {
+        PackageBox.getBuilder().length(1).width(0).depth(1).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exception_iae_depth_0() {
+        PackageBox.getBuilder().length(1).width(1).depth(0).build();
+    }
+
+    @Test
+    public void packageIsBuilt() {
+        PackageBox box = PackageBox.getBuilder().length(1).width(2).depth(3).build();
+        Assert.assertEquals(1, box.getLength(), 0);
+        Assert.assertEquals(2, box.getWidth(), 0);
+        Assert.assertEquals(3, box.getDepth(), 0);
     }
 }
